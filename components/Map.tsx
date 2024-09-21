929c1e2e52c0866ac2292573bbba4e9f74aca48b
Mike👨‍💻 <binarybardcode@gmail.com>
2024-09-21 16:12:59 +0200

    Implement Maps functionality

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from "react-native"
+import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+ //  const region = {}
+
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={true}
+    //  initialRegion={region} 
+    showsUserLocation={true}
+    userInterfaceStyle="light"
+    >
         <Text>Map</Text>
-    </View>
+    </MapView>
   )
 }
 
-export default Map
+export default Map 
